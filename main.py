be0e32a2b4321de33b16d49cf6ab1ab78c8f87ec
SirZen476 <danielzenkin82@gmail.com>
2024-10-06 16:10:56 +0300

    basic window created, added Game class in main, added player,py file for player

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+#this project uses files by CodeClear - https://github.com/clear-code-projects/5games
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import pygame
+from Tools.scripts.dutree import display
+from fontTools.merge.util import current_time
+from sympy.core.random import randint
+from random import randint, uniform
+from pygame.sprite import Sprite
+from settings import *
+
+class Game():
+    def __init__(self):
+        #pygame.init()# init pygame, causes freeze at start - neet to see if something goes wrong
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))# create screen
+        pygame.display.set_caption('Vamp Survivor')
+        self.clock = pygame.time.Clock()  # control framerate, control
+        self.running = True
+        self.FPS_target = FPS_TARGET
+
+    def gameloop(self):
+        while self.running:
+            #dt calc
+            dt = self.clock.tick(self.FPS_target) /1000
+            #event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                self.screen.fill('blue')
+
+            #update
+
+            #draw
+            pygame.display.update()  # or flip - flip updates a part of the window , update the whole window
+        pygame.quit()
+
+class Player(Sprite):
+    def __init__(self,groups):
+        super().__init__(groups)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':# to run only main file to avoid future messups
+    game = Game()
+    game.gameloop()
