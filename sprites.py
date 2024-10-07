c2e5e1a55e47879a82e7946725be71bc98a49a3a
SirZen476 <danielzenkin82@gmail.com>
2024-10-07 17:23:20 +0300

    gun now changes angle

--- sprites.py
+++ sprites.py
@@ -28,6 +28,7 @@ class Gun(Sprite):
         self.player = player
         self.distance = 140
         self.player_direction = pygame.Vector2(1,0)
+        self.angle = 0
         # sprite setup
         super().__init__(groups)
         self.gun_surface = pygame.image.load(join('images','gun','gun.png')).convert_alpha()
@@ -35,5 +36,13 @@ class Gun(Sprite):
         self.image = self.gun_surface
         self.rect = self.image.get_rect(center = self.player.rect.center + self.player_direction* self.distance)
 
+    def get_direction(self):
+        #mouse_pos = pygame.Vector2(pygame.mouse.get_pos()) # mouse pos
+        #player_pos = WINDOW_WIDTH/2 ,WINDOW_HEIGHT/2 # player always in middle of screen
+        return (pygame.Vector2(pygame.mouse.get_pos()) - (WINDOW_WIDTH/2 ,WINDOW_HEIGHT/2)).normalize()
+
     def update(self,_):
+        self.player_direction = self.get_direction()
+        self.angle = self.player_direction.angle_to(pygame.Vector2(1,0))
+        self.image = pygame.transform.rotate(self.gun_surface,self.angle)
         self.rect.center=self.player.rect.center + self.player_direction * self.distance
